03d4995096452c8fbaf7ab6f6ea0cc2520f49014
Devin Farmer <devin.farmer@gmail.com>
2016-07-09 21:15:23 +0900

    Finally figured out how to make Devise log out. I'm getting there, I guess

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,20 @@
 Rails.application.routes.draw do
   resources :places
-  devise_for :users
+  
+
+
+  devise_for :users 
+
+  devise_scope :user do 
+    get 'sign_out' => 'user/sessions#destroy' 
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
- root 'places#index'
+  root 'places#index'
+  
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
