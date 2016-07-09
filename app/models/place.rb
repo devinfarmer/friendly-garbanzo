aa5b087ad5304ad31efd1b140dd89f7ddb5e2f79
Devin Farmer <devin.farmer@gmail.com>
2016-07-09 19:08:37 +0900

    Associated User and Place

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,4 @@
 class Place < ActiveRecord::Base
 
-	belongs_to :user
+	has_and_belongs_to_many :users
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160707083126) do
+ActiveRecord::Schema.define(version: 20160709095341) do
 
   create_table "places", force: :cascade do |t|
     t.string   "name"
@@ -30,6 +30,11 @@ ActiveRecord::Schema.define(version: 20160707083126) do
     t.text     "comments"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "places_users", id: false, force: :cascade do |t|
+    t.integer "user_id",  null: false
+    t.integer "place_id", null: false
   end
 
   create_table "users", force: :cascade do |t|
